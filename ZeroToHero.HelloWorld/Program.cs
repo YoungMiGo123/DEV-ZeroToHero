37d4e13d6e66fd2f5e4a8c9ddb9e789f8e7dd1c2
martinkleinbooi@gmail.com <jackdaniels@123>
2024-01-19 16:29:40 +0200

    Initial

---
+++ ZeroToHero.HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿// New School 
+
+//Console.WriteLine("Hello, World!");
+
+// Old school 
+
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+
+
+
