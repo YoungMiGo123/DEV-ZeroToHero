156620c0415d2770b3204e43653e1c0756d45a47
martinkleinbooi@gmail.com <jackdaniels@123>
2024-01-20 16:33:08 +0200

    - Added OOP Module - Added TicTac toe game - Added new exercises - Added new tests

---
+++ ZeroToHero.OOP.Console/MessagePassing.cs
@@ -0,0 +1,22 @@
+﻿namespace ZeroToHero.OOP.Console
+{
+    using System;
+
+    public class MessagePassing
+    {
+        public class Messenger
+        {
+            public void DisplayMessage(string message)
+            {
+                Console.WriteLine($"Received message: {message}");
+            }
+        }
+
+        public static void BuildExample()
+        {
+            var messenger = new Messenger();
+            messenger.DisplayMessage("Hello from another class!");
+        }
+    }
+
+}
---
+++ ZeroToHero.TicTacToe/Models/Board.cs
@@ -0,0 +1,67 @@
+﻿namespace ZeroToHero.TicTacToe.Models
+{
+    public class Square 
+    {
+        public char Symbol { get; set; } 
+        public Square()
+        {
+            Symbol = '\0';
+        }
+        public bool IsTaken()
+        {
+            return Symbol != '\0';
+        }
+        public void SetSquare(char symbol)
+        {
+            Symbol = symbol;
+        }
+    }
+
+    public class Board
+    {
+        public Square[,] Squares { get; set; }
+        public int Rows => 3;
+        public int Cols => 3;
+        public Board()
+        {
+            Squares = new Square[Rows,Cols];
+            InitializeBoard();  
+        }
+        private void InitializeBoard()
+        {
+            for (int i = 0; i < Rows; i++)
+            {
+                for (int j = 0; j < Cols; j++)
+                {
+                    Squares[i, j] = new Square();
+                }
+            }
+        }
+        public bool IsFull()
+        {
+            for (int i = 0; i < Rows; i++)
+            {
+                for (int j = 0; j < Cols; j++)
+                {
+                    if (Squares[i, j].IsTaken())
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        public bool IsSquareTaken(int row, int col)
+        {
+            return Squares[row, col].IsTaken();
+        }
+        public void SetSquare(TicTacToeModel ticTacToeModel)
+        {
+            if (!IsSquareTaken(ticTacToeModel.Row, ticTacToeModel.Col))
+            {
+                Squares[ticTacToeModel.Row, ticTacToeModel.Col].SetSquare(ticTacToeModel.Symbol);
+            }
+         
+        }
+    }
+}
